262facaa29e4c85d7bd16972b2c47302ddcaf9e7
wangchong10 <wangchong10@guazi.com>
2020-04-15 19:00:51 +0800

    add： 追加hoc

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <base-component>
+            <h2 slot='slot1'>BaseComponent slot</h2>
+            <p>default slot</p>
+        </base-component>
+        <hr>
+        <enhanced-com>
+            <h2 slot='slot1'>EnhancedComponent slot</h2>
+            <p>default slot</p>
+        </enhanced-com>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import BaseComponent from './components/BaseComponent.vue'
+    import hoc from './hoc.js'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    const EnhancedCom = hoc(BaseComponent)
+
+    export default {
+        components: {
+            BaseComponent,
+            EnhancedCom
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ demo/src/components/BaseComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <span @click='handleClick'>props: {{test}}</span>
+        <!-- 具名插槽 -->
+        <slot name='slot1'/>
+        <p>===========</p>
+        <!-- 默认插槽 -->
+        <slot/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BaseComponent',
+        props: {
+            test: Number
+        },
+        methods: {
+            handleClick () {
+                this.$emit('customize-click')
+            }
+        }
+    }
+</script>
